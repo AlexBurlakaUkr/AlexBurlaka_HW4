a64a8224edf875de0906fc3d8c3475dfab7db605
Alex Burlaka <alexburlakaukr@gmail.com>
2021-10-31 22:26:48 +0200

    honest and odd arrays

--- 20211030_HW4/20211030_HW4/Program.cs
+++ 20211030_HW4/20211030_HW4/Program.cs
@@ -16,6 +16,8 @@ namespace HW4
             Console.WriteLine("Enter the size of array");
             int sizearray = int.Parse(Console.ReadLine());
             int[] arrayRandom = new int[sizearray];
+            int[] honestarray = new int[sizearray];
+            int[] oddarray = new int[sizearray];
             Random rmd = new Random();
             var min = 1;
             var max = 26;
@@ -26,6 +28,30 @@ namespace HW4
 
             string a = string.Join(" ", arrayRandom);
             Console.WriteLine(a);
+            for (int i = 0; i < arrayRandom.Length; i++)
+            {
+                if (arrayRandom[i] % 2 == 0)
+                {
+                    honestarray[i] = arrayRandom[i];
+                }
+                else
+                {
+                    oddarray[i] = arrayRandom[i];
+                }
+            }
+
+            int[] honestarraywithoutzero = Array.FindAll(honestarray, i => i > 0);
+            foreach (var i in honestarraywithoutzero)
+            {
+                Console.WriteLine(i);
+            }
+
+            int[] oddarraywithoutzero = Array.FindAll(oddarray, i => i > 0);
+            foreach (var i in oddarraywithoutzero)
+            {
+                Console.WriteLine(i);
+            }
+
             /*foreach (var i in arrayRandom)
             {
                 Console.WriteLine(i);
