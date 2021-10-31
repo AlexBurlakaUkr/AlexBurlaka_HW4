6cc99238bda2b9f3def4d69deb1f5e24635b171f
Alex Burlaka <alexburlakaukr@gmail.com>
2021-10-31 21:06:58 +0200

    the first stage of the task -  the input of the array

--- 20211030_HW4/20211030_HW4/Program.cs
+++ 20211030_HW4/20211030_HW4/Program.cs
@@ -1,11 +1,35 @@
 ﻿using System;
 
-namespace _20211030_HW4
+namespace HW4
 {
-    class Program
+    /// <summary>
+    /// Main program.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Messeg into console.
+        /// </summary>
+        /// <param name="args">console call params.</param>
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Enter the size of array");
+            int sizearray = int.Parse(Console.ReadLine());
+            int[] arrayRandom = new int[sizearray];
+            Random rmd = new Random();
+            var min = 1;
+            var max = 26;
+            for (int i = 0; i < arrayRandom.Length; i++)
+            {
+                arrayRandom[i] = rmd.Next(min, max);
+            }
+
+            string a = string.Join(" ", arrayRandom);
+            Console.WriteLine(a);
+            /*foreach (var i in arrayRandom)
+            {
+                Console.WriteLine(i);
+            }*/
         }
     }
 }
